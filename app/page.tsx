c953cc8061719dc9546aa532534b8d561e9ecbbc
Lucas Santos <jl.lucassantos098@gmail.com>
2023-07-06 18:00:18 -0300

    img

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,13 @@ export default function Page() {
 		<main className='bg-white'>
 			<div className="flex justify-center items-center h-screen">
 				<div className="bg-gray-100 p-4 m-4 rounded shadow-md text-center">
-					<Image width={350} height={350} src="./next.svg" alt="Imagem" className="mx-auto" />
+					<Image width={350} height={350} src="/img.jpg" alt="Imagem" className="mx-auto" />
 					<button className="mt-4 bg-blue-500 text-white p-2 rounded">
 						Login
 					</button>
 				</div>
 				<div className="bg-gray-100 p-4 m-4 rounded shadow-md text-center">
-					<Image width={350} height={350} src="./next.svg" alt="Imagem" className="mx-auto" />
+					<Image width={350} height={350} src="/img.jpg" alt="Imagem" className="mx-auto" />
 					<button className="mt-4 bg-blue-500 text-white p-2 rounded">
 						Login
 					</button>
