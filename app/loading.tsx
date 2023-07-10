7aef912c1192fa0e4e57671d8eff3639584b7818
Lucas Santos <jl.lucassantos098@gmail.com>
2023-07-10 17:33:43 -0300

    loading

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+import '@/styles/loading.css'
+export default function PageLoading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  )
+}
